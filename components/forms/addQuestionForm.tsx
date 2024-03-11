02a511eacf28b61a7559434a0ddd04e68b612631
Mohamed Hosam <mohamedhosaam154@gmail.com>
2024-03-11 03:07:14 +0300

    adding category

--- components/forms/addQuestionForm.tsx
+++ components/forms/addQuestionForm.tsx
@@ -2,9 +2,42 @@ import React, { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import toast, { Toaster } from "react-hot-toast";
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const FormSchema = z.object({
+  category: z.string({
+    required_error: "Please select category.",
+  }),
+  question: z.string({
+    required_error: "Please add question.",
+  }),
+});
 
 export default function AddQuestionForm() {
   const [question, setQuestion] = useState<string>();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
 
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -14,24 +47,66 @@ export default function AddQuestionForm() {
       console.log(question);
     }
   };
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   return (
-    <form
-      className="flex flex-col items-center justify-between gap-8 mt-4"
-      onSubmit={handleFormSubmit}
-    >
-      <Toaster position="bottom-right" reverseOrder={false} />
+    <Form {...form}>
+      <form
+        className="flex flex-col  items-center justify-between gap-6 mt-4"
+        onSubmit={form.handleSubmit(onSubmit)}
+        // onSubmit={handleFormSubmit}
+      >
+        <Toaster position="bottom-right" reverseOrder={false} />
 
-      <h2 className="text-2xl font-semibold leading-none tracking-tight text-center">
-        Add Question
-      </h2>
-      <Textarea
-        placeholder="Enter your question."
-        onChange={(e) => setQuestion(e.target.value)}
-        className="h-64"
-        required
-      />
+        <h2 className="text-2xl font-semibold leading-none tracking-tight text-center">
+          Add Question
+        </h2>
+        <FormField
+          control={form.control}
+          name="question"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel>Question</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Enter your question."
+                  className="h-36 resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="category"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel>Category</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select question category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="Math">Math</SelectItem>
+                  <SelectItem value="Physics">Physics</SelectItem>
+                  <SelectItem value="Chemistry">Chemistry</SelectItem>
+                </SelectContent>
+              </Select>
 
-      <Button className="mx-auto w-40">Add Question</Button>
-    </form>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="mx-auto w-40">Add Question</Button>
+      </form>
+    </Form>
   );
 }
